b85d7066261fac91ae9dab6443ac89a7974362b0
Habl <habltaker@gmail.com>
2025-07-18 20:15:42 +0300

    fix

---
+++ src/js/main.js
@@ -0,0 +1,41 @@
+import { getImagesByQuery } from './pixabay-api';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './render-functions';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', async e => {
+  e.preventDefault();
+  const query = form.elements['search-text'].value.trim();
+
+  if (!query) {
+    iziToast.warning({ message: 'Please enter a search term', position: 'topRight' });
+    return;
+  }
+
+  showLoader();
+  clearGallery();
+
+  try {
+    const data = await getImagesByQuery(query);
+
+    if (data.hits.length === 0) {
+      iziToast.error({
+        message: 'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+      });
+    } else {
+      createGallery(data.hits);
+    }
+  } catch (error) {
+    iziToast.error({ message: 'Failed to fetch images', position: 'topRight' });
+  } finally {
+    hideLoader();
+  }
+});
